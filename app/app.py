60f03ba4453dc6e7f1825b8b27fbe41ea4ef151d
Siddhant Jain <siddhantjain209@gmail.com>
2023-06-22 23:20:48 +0530

    add return 404 if playlist link invalid or private

--- app/app.py
+++ app/app.py
@@ -1,20 +1,33 @@
 from flask import Flask, request, jsonify
 from providers.scraper import *
+import json
+import sys
 
 app = Flask(__name__)
 scraper = Scraper()
 
+@app.route("/hi")
+def hello():
+    return jsonify({"hi": "hello"})
+
 @app.route("/playlist", methods=['POST'])
 def get_playlist_as_csv():
-    playlistUrl = request.form.get('playlist_url')
+    data = request.get_json()
+    playlistUrl = data.get("playlist_url")
     message = "csv has been saved"
     try:
         scraper.get_playlist(playlistUrl)
+        csvData = scraper.save_trackdata_csv()
     except:
-        message = "some error occured"
-
-    csvData = scraper.save_trackdata_csv()
+        message = "Playlist not found"
+        return jsonify({
+            'message': message
+        }), 404
 
     return jsonify({
-        'message': csvData
+        'message': message,
+        'csv': csvData
     })
+
+if __name__ == "__main__":
+    app.run(debug=True)
